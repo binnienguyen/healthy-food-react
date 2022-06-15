bdb27342f52a157f0c02f35d05017d310ada9a92
Binnie <lananh4415@gmail.com>
2022-06-15 17:21:39 +0700

    init-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/header/Header";
+import {BestServices} from "./components/main/best-services/BestServices";
+import {Blog} from "./components/main/blog/Blog";
+import {Contact} from "./components/main/contact/Contact";
+import {Hero} from "./components/main/hero/HeroSection";
+import {BestRecipes} from "./components/main/best-recipes/BestRecipes";
+import {Footer} from "./components/footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+    // );
+    return (
+        <>
+            <Header/>
+
+            <Hero/>
+            <BestRecipes />
+            <BestServices/>
+            <Blog/>
+            <Contact/>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/main/best-recipes/post/Post.jsx
@@ -0,0 +1,17 @@
+import "./PostStyle.css";
+import "./ResponsivePost.css";
+
+export const RecipePost = (props) => {
+    return (
+        <div className="postWrapper">
+            <div className="leftInfo">
+                <img src={props.src} alt={props.alt}/>
+
+            </div>
+            <div className="rigthInfo">
+                <h2>{props.title}</h2>
+                <button>See Recipe</button>
+            </div>
+        </div>
+    )
+}
